65a806f00109e9ebaa2d4a6466819e7a83195a23
Ali Norouzi <anorouziiii@gmail.com>
2024-05-16 12:02:45 +0330

    feat: Add menu section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,5 +9,23 @@
       navList.classList.toggle('show');
       toggleButton.classList.toggle('active');
     });
+    // Menu tabs
+    const tabs = document.querySelectorAll('.menu__tab');
+    const tabContents = document.querySelectorAll('.menu__content');
+
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        const tabId = tab.getAttribute('data-tab');
+        tabContents.forEach(content => {
+          content.classList.remove('active');
+        });
+        tabs.forEach(t => {
+          t.classList.remove('active');
+        });
+        document.getElementById(tabId).classList.add('active');
+        tab.classList.add('active');
+      });
+    });
+
   });
 })();
